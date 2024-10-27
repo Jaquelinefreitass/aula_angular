23ca0c73950a15d0c6af1777965cac1066d9beb3
Jaqueline <133174675+jueslyyy@users.noreply.github.com>
2024-10-27 18:43:01 -0300

    commit

---
+++ buscacep-ui/src/app/cnpj/cnpj.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cnpj',
+  templateUrl: './cnpj.component.html',
+  styleUrls: ['./cnpj.component.css']
+})
+export class CnpjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
